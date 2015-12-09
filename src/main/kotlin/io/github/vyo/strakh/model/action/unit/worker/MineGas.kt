506159c568917db2aa336110942988da120545b1
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-12-09 20:03:41 +0100

    Fix imports

--- src/main/kotlin/io/github/vyo/strakh/model/action/unit/worker/MineGas.kt
+++ src/main/kotlin/io/github/vyo/strakh/model/action/unit/worker/MineGas.kt
@@ -2,21 +2,21 @@ package io.github.vyo.strakh.model.action.unit.worker
 
 import io.github.vyo.strakh.goap.component.Action
 import io.github.vyo.strakh.goap.component.Agent
-import io.github.vyo.strakh.utility.Cost
 import io.github.vyo.strakh.model.agent.Unit
 import io.github.vyo.strakh.model.game.Resources
+import io.github.vyo.strakh.utility.Cost
 
 /**
  * Created by Manuel Weidmann on 22.11.2015.
  */
 
-class MineGas(val unit: io.github.vyo.strakh.model.agent.Unit) : io.github.vyo.strakh.goap.component.Action {
+class MineGas(val unit: Unit) : Action {
 
-    override var agent: io.github.vyo.strakh.goap.component.Agent = unit
-    override var cost: io.github.vyo.strakh.utility.Cost = io.github.vyo.strakh.utility.Cost(actions = 1, time = 100)
+    override var agent: Agent = unit
+    override var cost: Cost = Cost(actions = 1, time = 100)
 
     override fun applicable(): Boolean {
-        var spot: io.github.vyo.strakh.model.game.Resources.ResourceSpot = io.github.vyo.strakh.model.game.Resources.nearestMineralPatch(unit.unit)
+        var spot: Resources.ResourceSpot = Resources.nearestMineralPatch(unit.unit)
         val (geyser, nearby, saturated, hasRefinery) = spot
 
         return geyser != null && nearby && !saturated && hasRefinery
